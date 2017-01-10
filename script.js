05eab2ea8b9c38f29dd7f767ee9990f0d4ae2148
moniquedesalvo <moniquerenee@gmail.com>
2017-01-10 08:52:44 -0800

    media queries and hamburger menu for mobile

---
+++ script.js
@@ -0,0 +1,26 @@
+$( "#close" ).hide();
+$( ".mobile-menu" ).hide();
+
+$( "#hamburger" ).click(function() {
+	$( ".mobile-menu" ).slideToggle( "fast", function() {
+		$( "#hamburger" ).hide();
+		$( "#close" ).show();
+
+	});
+});
+
+$( "#close" ).click(function() {
+	$( ".mobile-menu" ).slideToggle( "fast", function() {
+		$( "#close" ).hide();
+		$( "#hamburger" ).show();
+	});
+});
+
+$( ".mobile-menu" ).click(function() {
+	$( ".mobile-menu" ).slideToggle( "fast", function() {
+		$( "#close" ).hide();
+		$( "#hamburger" ).show();
+		// $( "mobile-menu").hide();
+	});
+});	
+
